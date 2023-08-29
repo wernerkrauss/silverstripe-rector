519404d568b127562f7344f96dc3397c8c09330a
Werner Krauß <werner.krauss@netwerkstatt.at>
2023-08-29 10:52:25 +0200

    Stubs: DataExtension is a subclass of Extension

--- stubs/SilverStripe/ORM/DataExtension.php
+++ stubs/SilverStripe/ORM/DataExtension.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace SilverStripe\ORM;
+use SilverStripe\Core\Extension;
+
 if (class_exists('SilverStripe\ORM\DataExtension')) {
     return;
 }
-class DataExtension
+class DataExtension extends Extension
 {
-
+    public $owner;
+    public function getOwner()
+    {
+    }
 }
