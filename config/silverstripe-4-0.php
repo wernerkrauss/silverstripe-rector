db62877261b6684bb60dca5c509d9a1bd8500236
Werner Krauß <werner.krauss@netwerkstatt.at>
2023-09-14 10:20:11 +0200

    add missing files

--- config/silverstripe-4-0.php
+++ config/silverstripe-4-0.php
@@ -10,6 +10,7 @@ use Rector\Transform\ValueObject\ParentClassToTraits;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(EnsureTableNameIsSetRector::class);
 
+    //pre 4.0
     $object_traits = [
         'SilverStripe\Core\Injector\Injectable',
         'SilverStripe\Core\Config\Configurable',
@@ -22,4 +23,42 @@ return static function (RectorConfig $rectorConfig): void {
             new ParentClassToTraits('SS_Object', $object_traits),
         ]
     );
+
+
+    //4.0 https://docs.silverstripe.org/en/4/changelogs/4.0.0/#dataobject-versioned
+    //Versioned::publish Replaced by Versioned::copyVersionToStage
+
+    //Versioned::doPublish Replaced by Versioned::publishRecursive
+
+    //The Config::inst()->update() method is deprecated, and replaced with Config::modify()->set() and Config::modify()->merge() to respectively replace and merge config.
+
+    //https://docs.silverstripe.org/en/4/changelogs/4.0.0/#overview-general
+
+    //SiteTree.alternatePreviewLink is deprecated. Use updatePreviewLink instead.
+
+    //Deprecated Member::checkPassword(). Use Authenticator::checkPassword() instead
+
+    //Deprecated ClassInfo::baseDataClass(). Use DataObject::getSchema()->baseDataClass() instead.
+
+    //Deprecated ClassInfo::table_for_object_field(). Use DataObject::getSchema()->tableForField() instead
+
+    //https://docs.silverstripe.org/en/4/changelogs/4.0.0/#overview-orm
+    //Deprecated SQLQuery in favour SQLSelect
+
+    //Renamed String::NoHTML() to Plain()
+
+    //Removed String::LimitWordCountXML(). Use LimitWordCount() instead.
+
+    //Removed String::BigSummary(). Use Summary() instead.
+
+    //Renamed FormField#createTag() to FormField::create_tag()
+
+    //Permission::flush_permission_cache() renamed to reset() and added to Resettable interface.
+
+    //Renamed Versioned::reading_stage() to set_stage() (throws an error if setting an invalid stage)
+    //Renamed Versioned::current_stage() to get_stage()
+    //Removed Versioned::get_live_stage(). Use the Versioned::LIVE constant instead.
+    //Removed Versioned::getDefaultStage(). Use the Versioned::DRAFT constant instead.
+
+
 };
--- config/silverstripe-5-0.php
+++ config/silverstripe-5-0.php
@@ -5,6 +5,45 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
+    //deprecations cms https://docs.silverstripe.org/en/4/changelogs/4.13.0/#silverstripe-cms
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigatorItem_Unversioned Will be renamed SilverStripe\Admin\Navigator\SilverStripeNavigatorItem_Unversioned
 
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigatorItem_StageLink Will be renamed SilverStripe\VersionedAdmin\Navigator\SilverStripeNavigatorItem_StageLink
 
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigator Will be renamed SilverStripe\Admin\Navigator\SilverStripeNavigator
+
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigatorItem_LiveLink Will be renamed SilverStripe\VersionedAdmin\Navigator\SilverStripeNavigatorItem_LiveLink
+
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigatorItem_ArchiveLink Will be renamed SilverStripe\VersionedAdmin\Navigator\SilverStripeNavigatorItem_ArchiveLink
+
+    //Deprecated class SilverStripe\CMS\Controllers\SilverStripeNavigatorItem Will be renamed SilverStripe\Admin\Navigator\SilverStripeNavigatorItem
+
+    //deprecations framework https://docs.silverstripe.org/en/4/changelogs/4.13.0/#silverstripe-framework
+    //Deprecated class SilverStripe\Dev\CSVParser Use League\Csv\Reader instead
+
+    //Deprecated class SilverStripe\View\Parsers\Diff Will be replaced with SilverStripe\View\Parsers\HtmlDiff
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::rewind() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::current() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::first() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::key() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::next() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::nextRecord() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\Query::seek() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\MySQLStatement::seek() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\MySQLStatement::nextRecord() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\MySQLQuery::seek() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\ORM\Connect\MySQLQuery::nextRecord() Will be replaced by getIterator() in CMS 5
+
+    //Deprecated method SilverStripe\View\Parsers\Diff::compareHTML() Will be replaced with SilverStripe\View\Parsers\HtmlDiff::compareHTML()
 };
