83a1ad43ee5bdcd64ae6b018f850e2d220a5240f
Florian Thoma <florian.thoma@innoweb.com.au>
2025-04-10 20:19:18 +1000

    upgrade to rector 2, add SS 5.3 and 5.4 updates

--- config/silverstripe-5-3.php
+++ config/silverstripe-5-3.php
@@ -3,8 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Renaming\Rector\Name\RenameClassRector;
 
 return static function (RectorConfig $rectorConfig): void {
-
-
+    $rectorConfig->ruleWithConfiguration(RenameClassRector::class, [
+        'SilverStripe\ORM\DataExtension' => 'SilverStripe\Core\Extension',
+        'SilverStripe\CMS\Model\SiteTreeExtension' => 'SilverStripe\Core\Extension',
+        'SilverStripe\Admin\LeftAndMainExtension' => 'SilverStripe\Core\Extension',
+    ]);
+    $rectorConfig->importNames(removeUnusedImports: true);
 };
