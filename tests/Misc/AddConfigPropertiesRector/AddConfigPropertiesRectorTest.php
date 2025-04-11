3374b6ec3f279bc01d7ea673aeb19a35f91861c8
Florian Thoma <florian.thoma@innoweb.com.au>
2025-04-11 11:38:53 +1000

    update tests to phpunit 11

--- tests/Misc/AddConfigPropertiesRector/AddConfigPropertiesRectorTest.php
+++ tests/Misc/AddConfigPropertiesRector/AddConfigPropertiesRectorTest.php
@@ -5,20 +5,19 @@ declare(strict_types=1);
 namespace Netwerkstatt\SilverstripeRector\Tests\Misc\AddConfigPropertiesRector;
 
 use Rector\Testing\PHPUnit\AbstractRectorTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class AddConfigPropertiesRectorTest extends AbstractRectorTestCase
 {
-    /**
-     * @dataProvider provideData()
-     */
+    #[DataProvider('provideData')]
     public function test(string $filePath): void
     {
         $this->doTestFile($filePath);
     }
 
-    public function provideData(): \Iterator
+    public static function provideData(): \Iterator
     {
-        return $this->yieldFilesFromDirectory(__DIR__ . '/Fixture');
+        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
     }
 
     public function provideConfigFilePath(): string
