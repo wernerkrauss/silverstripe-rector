532235e2b2fe70810bba336d67d218b05bbc3d20
Werner Krauß <werner.krauss@netwerkstatt.at>
2025-07-31 22:55:36 +0200

    fix docs

--- src/Rector/Misc/RenameAddFieldsToTabWithoutArrayParamRector.php
+++ src/Rector/Misc/RenameAddFieldsToTabWithoutArrayParamRector.php
@@ -9,8 +9,6 @@ use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\NullsafeMethodCall;
 use PhpParser\Node\Name;
 use Rector\Rector\AbstractRector;
-use Rector\ValueObject\PhpVersionFeature;
-use Rector\VersionBonding\Contract\MinPhpVersionInterface;
 use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
@@ -20,12 +18,11 @@ final class RenameAddFieldsToTabWithoutArrayParamRector extends AbstractRector i
     public function getRuleDefinition() : RuleDefinition
     {
         return new RuleDefinition('Renames ->addFieldsToTab($name, $singleField) to ->addFieldToTab($name, $singleField)', [new CodeSample(<<<'CODE_SAMPLE'
-class SomeClass
+class SomeClass extends \SilverStripe\ORM\DataObject
 {
-    public function getCMSFields()
-    {
-        $time = mktime(1, 2, 3);
-        $nextTime = mktime();
+    public function getCMSFields() {
+        $myfield = FormField::create();
+        $fields->addFieldsToTab('Root.Main', $myfield);
     }
 }
 CODE_SAMPLE
