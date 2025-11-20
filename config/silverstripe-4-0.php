8c88207b0c556a33120d50cd23c4799fe69dfad8
Andy Adiwidjaja <adiwidjaja@gmail.com>
2025-11-20 17:37:53 +0100

    Removed deprecated rector functionality from silverstripe-4-0 config.

--- config/silverstripe-4-0.php
+++ config/silverstripe-4-0.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 use Netwerkstatt\SilverstripeRector\Rector\DataObject\EnsureTableNameIsSetRector;
 use Rector\Config\RectorConfig;
-use Rector\Transform\Rector\Class_\ParentClassToTraitsRector;
+// use Rector\Transform\Rector\Class_\ParentClassToTraitsRector;
 use Rector\Transform\ValueObject\ParentClassToTraits;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(EnsureTableNameIsSetRector::class);
 
     //pre 4.0
-    $object_traits = [
-        'SilverStripe\Core\Injector\Injectable',
-        'SilverStripe\Core\Config\Configurable',
-        'SilverStripe\Core\Extensible'
-    ];
-    $rectorConfig->ruleWithConfiguration(
-        ParentClassToTraitsRector::class,
-        [
-            new ParentClassToTraits('Object', $object_traits),
-            new ParentClassToTraits('SS_Object', $object_traits),
-        ]
-    );
+    // $object_traits = [
+    //     'SilverStripe\Core\Injector\Injectable',
+    //     'SilverStripe\Core\Config\Configurable',
+    //     'SilverStripe\Core\Extensible'
+    // ];
+    // $rectorConfig->ruleWithConfiguration(
+    //     ParentClassToTraitsRector::class,
+    //     [
+    //         new ParentClassToTraits('Object', $object_traits),
+    //         new ParentClassToTraits('SS_Object', $object_traits),
+    //     ]
+    // );
 
 
     //4.0 https://docs.silverstripe.org/en/4/changelogs/4.0.0/#dataobject-versioned
